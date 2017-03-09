1808c2cca7172fd53c8a71d7dc4809fb964c876a
Kevin Zhang <whitehat2k9@gmail.com>
2017-03-09 11:27:45 -0500

    Add sump_alert module

---
+++ sump_alert.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from email.mime.text import MIMEText
+import smtplib
+import time
+
+from sump_web import app, get_db
+
+
+CHECK_INTERVAL = 3600
+ALERT_INTERVAL = 300
+
+def send_alert(v):
+    v = round(v, 1)
+    subject = "ALERT: {} water depth is now at {} cm".format(app.config['NAME'], v)
+    body = "{}'s average water depth over the past {} seconds is now at {} cm!"
+    body = body.format(app.config['NAME'], CHECK_INTERVAL, v)
+    msg = MIMEText(body)
+    msg['From'] = app.config['GMAIL_USERNAME']
+    msg['To'] = ', '.join(app.config['ALERT_RECIPIENTS'])
+    msg['Subject'] = subject
+    gm = smtplib.SMTP('smtp.gmail.com', 587)
+    gm.starttls()
+    gm.ehlo()
+    gm.login(app.config['GMAIL_USERNAME'], app.config['GMAIL_PASSWORD'])
+    gm.send_message(msg)
+    gm.quit()
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db = get_db()
+        last_alert_time = 0
+        while True:
+            cur = db.execute("SELECT AVG(value) FROM data WHERE timestamp >= datetime('now', 'unixepoch') - ?", [CHECK_INTERVAL])
+            if cur.rowcount:
+                avg = cur.fetchone()[0]
+                if avg >= app.config['ALERT_DEPTH'] and int(time.time()) - CHECK_INTERVAL > last_alert_time:
+                    try:
+                        send_alert(avg)
+                    except Exception as e:
+                        print(e)
+                    else:
+                        last_alert_time = int(time.time())
+            time.sleep(CHECK_INTERVAL)
--- sump_logger.py
+++ sump_logger.py
@@ -9,6 +9,7 @@ from sump_web import app, get_db
 
 if __name__ == '__main__':
     with app.app_context():
+        time.sleep(5)
         db = get_db()
         ser = serial.Serial(app.config['SERIAL_DEVICE'], 115200)
         while True:
